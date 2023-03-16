44934bbaab4afb9ed96bb69db8377444f5161c40
Islam Hany <ihhg.developer@gmail.com>
2023-03-16 10:32:29 +0200

    add 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n
+		Size of an int: %d byte(s)\n
+		Size of a long int: %d byte(s)\n
+		Size of a long long int: %d byte(s)\n
+		Size of a float: %d byte(s)\n"
+		, sizeof(ch), sizeof(i), sizeof(li), sizeof(lli), sizeof(f));
+
+	return (0);
+}
