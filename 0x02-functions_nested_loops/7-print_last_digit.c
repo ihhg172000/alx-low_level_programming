8851c4c813b5fa8389edefb4fd618f5580be0792
Islam Hany <ihhg.developer@gmail.com>
2023-03-21 22:53:12 +0200

    another fix for 7-print_last_digit.c flie

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,14 @@
 */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	if (n < 0)
+		last_digit = (n % 10) * -1;
+	else
+		last_digit = n % 10;
 
 	_putchar(last_digit + 48);
+
 	return (last_digit);
 }
