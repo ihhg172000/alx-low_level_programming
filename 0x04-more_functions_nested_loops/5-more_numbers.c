3b06ef1064f3f78fb30ee3a1ea338d98624ebadf
Islam Hany <ihhg.developer@gmail.com>
2023-03-24 00:36:54 +0200

    fix 5-more_numbers.c file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,14 @@ void more_numbers(void)
 {
 	int count, n;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n <= 9)
-				_putchar(n + 48);
-			else
-			{
-				_putchar((n / 10) + 48);
-				_putchar((n % 10) + 48);
-			}
+			if (n >= 10)
+				_putchar(1 + 48);
+
+			_putchar((n % 10) + 48);
 		}
 
 		_putchar('\n');
