f1b75e63ac38cb600ed5cd588893cc27f4b5c29b
Islam Hany <ihhg.developer@gmail.com>
2023-03-24 13:25:12 +0200

    fix 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,23 @@
 #include "main.h"
 
-int _abs(int n);
-
 /**
 * print_number - prints an integer
 * @n: the integer number to be printed
 */
 void print_number(int n)
 {
-	int abs_value = _abs(n);
-	int base = 1;
-
-	while (abs_value / (base * 10) != 0)
-		base *= 10;
+	unsigned int number;
 
 	if (n < 0)
-		_putchar('-');
-
-	while (base >= 1)
 	{
-		_putchar((abs_value / base) % 10 + 48);
-		base /= 10;
+		number = n * -1;
+		_putchar('-');
 	}
-}
-
-/**
-* _abs - gets the absolute value of given number
-* @n: the number to get its absolute value
-*
-* Return: absolute value of given number
-*/
-int _abs(int n)
-{
-	if (n < 0)
-		return (n * -1);
 	else
-		return (n);
+		number = n;
+
+	if ((number / 10) != 0)
+		print_number(number / 10);
+
+	_putchar((number % 10) + '0');
 }
