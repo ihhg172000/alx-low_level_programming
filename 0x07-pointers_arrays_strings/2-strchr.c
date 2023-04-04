296c2c284d871904f0c8c68a50626f984858e0c5
Islam Hany <ihhg.developer@gmail.com>
2023-04-04 02:07:08 +0200

    add 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+* _strchr - function that locates a character in a string
+* @s: pointer to string
+* @c: character to locate it
+*
+* Return: a pointer to the first occurrence of the character c
+*/
+char *_strchr(char *s, char c)
+{
+	char *temp = s;
+
+	while (*temp)
+	{
+		if (*temp == c)
+			return (temp);
+
+		temp++;
+	}
+
+	return (0);
+}
