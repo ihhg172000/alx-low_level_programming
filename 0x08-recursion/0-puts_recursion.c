2850212342bb898e6f550883b5f5627b115df2ea
Islam Hany <ihhg.developer@gmail.com>
2023-04-05 19:00:40 +0200

    add 1-print_rev_recursion.c file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _puts_recursion - unction that prints a string, followed by a new line
+* _puts_recursion - function that prints a string, followed by a new line
 * @s: string to be printed
 */
 void _puts_recursion(char *s)
