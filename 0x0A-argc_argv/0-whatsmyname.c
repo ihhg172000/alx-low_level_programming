89f3b81ccfb2ba2dc3f1adee74fb8e934c4e6e9c
Islam Hany <ihhg.developer@gmail.com>
2023-04-08 10:31:55 +0200

    add 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - program that prints its name
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 allways
+*/
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
