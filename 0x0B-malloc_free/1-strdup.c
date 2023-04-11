b75eb924d9126bbf6342b376c01fe774a82340ca
Islam Hany <ihhg.developer@gmail.com>
2023-04-11 13:02:39 +0200

    add 1-strdup.c function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+* _strdup - function copies a string to a new memory block
+* @str: pointer to a string to be copied
+*
+* Return: pointer to a newly allocated memory block
+*/
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int size = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		size++;
+
+	ptr = (char *) malloc(size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		ptr[i] = str[i];
+
+	return (ptr);
+}
