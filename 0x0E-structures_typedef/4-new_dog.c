e6cd76743bcc09a10ac5836ded07f5d2e96c1a31
Islam Hany <ihhg.developer@gmail.com>
2023-04-18 01:48:44 +0200

    add 4-new_dog.c function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+* new_dog - function that creates a new dog
+* @name: string represents name of the dog
+* @age: string represents age of the dog
+* @owner: string represents name of the owner
+*
+* Return: pointer to dog structure
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *_name, *_owner;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	_name = malloc((strlen(name) + 1));
+	if (_name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	_name = strcpy(_name, name);
+
+	_owner = malloc((strlen(owner) + 1));
+	if (_owner == NULL)
+	{
+		free(dog);
+		free(_name);
+		return (NULL);
+	}
+
+	_owner = strcpy(_owner, name);
+
+	dog->name = _name;
+	dog->age = age;
+	dog->owner = _owner;
+
+	return (dog);
+}
