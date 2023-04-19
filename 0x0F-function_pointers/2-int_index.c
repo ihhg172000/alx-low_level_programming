dea8bb9d3554a6c0aaf0af892fbbd57aee2838d8
Islam Hany <ihhg.developer@gmail.com>
2023-04-19 20:36:11 +0200

    fix 2-int_index.c function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
 * int_index - function searches for an integer
 * @array: array of integers
@@ -13,10 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
 
 	return (-1);
