8cf58ad59c3fa199d4d4e9e15d706d3ce8468873
Islam Hany <ihhg.developer@gmail.com>
2023-05-09 02:26:25 +0300

    fix cp program

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ void safe_close(int fd);
 */
 int main(int ac, char *av[])
 {
-	int fd_from, fd_to, br;
+	int fd_from, fd_to, br, bw;
 	char buff[BUFFSIZE];
 
 	if (ac != 3)
@@ -26,27 +26,24 @@ int main(int ac, char *av[])
 	}
 
 	fd_from = open(av[1], O_RDONLY);
-
-	if (fd_from < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-
-	fd_to = open(
-			av[2],
-			O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-	if (fd_to < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
-		exit(99);
-	}
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		br = read(fd_from, buff, BUFFSIZE);
-		write(fd_to, buff, br);
+
+		if (fd_from < 0 || br < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+
+		bw = write(fd_to, buff, br);
+
+		if (fd_to < 0 || bw < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
 	} while (br);
 
 	safe_close(fd_from);
@@ -57,7 +54,7 @@ int main(int ac, char *av[])
 
 /**
 * safe_close - function closes a file decriptor
-* @fd: the file decriptor
+* @fd: file decriptor
 */
 void safe_close(int fd)
 {
