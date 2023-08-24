2f4fbb6f5826f739920498a1cd752d5fe8a2c8e0
Islam Hany <ihhg.developer@gmail.com>
2023-08-24 04:25:22 +0300

    fix #3 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,28 +8,39 @@ def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid.
     """
+    new_grid = list(filter(lambda r: not all(n == 0 for n in r), grid))
+    for r in range (1, len(new_grid) - 1):
+        for c in range(len(new_grid[r])):
+            if new_grid[r][c] == 0 and 1 in new_grid[r][:c] and 1 in new_grid[r][c:]:
+                new_grid[r][c] = 1
+                
     perimeter = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                try:
-                    if grid[row - 1][col] == 0:
+                
+    for r in range(len(new_grid)): #0 4
+        for c in range(len(new_grid[r])): #0 6
+            if new_grid[r][c] == 1:
+                if r != 0:
+                    if new_grid[r - 1][c] == 0:
                         perimeter += 1
-                except IndexError:
+                else:
                     perimeter += 1
-                try:
-                    if grid[row + 1][col] == 0:
+
+                if r != len(new_grid) - 1:
+                    if new_grid[r + 1][c] == 0:
                         perimeter += 1
-                except IndexError:
+                else:
                     perimeter += 1
-                try:
-                    if grid[row][col - 1] == 0:
+
+                if c != 0:                    
+                    if new_grid[r][c - 1] == 0:
                         perimeter += 1
-                except IndexError:
+                else:
                     perimeter += 1
-                try:
-                    if grid[row][col + 1] == 0:
+
+                if c != len(new_grid[r]) - 1:
+                    if new_grid[r][c + 1] == 0:
                         perimeter += 1
-                except IndexError:
+                else:
                     perimeter += 1
+                
     return perimeter
